6cfddeb3a9a029ef38ec951c35ec95c9debdfa08
ryohey <info@codingcafe.jp>
2021-09-05 17:08:05 +0900

    Add WavetableOscillator

--- src/bypass-processor.js
+++ src/bypass-processor.js
@@ -2,6 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+class WavetableOscillator {
+  constructor(sample) {
+    this.sampleIndex = 0
+    this.sample = sample
+    this.isPlaying = false
+  }
+
+  play() {
+    this.isPlaying = true
+    this.sampleIndex = 0
+  }
+
+  stop() {
+    this.isPlaying = false
+  }
+
+  process(output) {
+    if (!this.isPlaying) {
+      return
+    }
+    for (let i = 0; i < output.length; ++i) {
+      output[i] = this.sample[this.sampleIndex++]
+      if (this.sampleIndex >= this.sample.length) {
+        this.sampleIndex = 0
+      }
+    }
+  }
+}
+
 /**
  * A simple bypass node demo.
  *
@@ -11,42 +40,28 @@
 class BypassProcessor extends AudioWorkletProcessor {
   constructor() {
     super()
-    this.isEnabled = false
-    this.samples = {} // {pitch number: buffer}
-    this.pitch = 0
+    this.oscillators = {} // {pitch number: WavetableOscillator}
+    this.currentOscillator = null
     this.port.onmessage = (e) => {
       console.log(e.data)
       switch (e.data.type) {
         case "noteOn":
-          this.isEnabled = true
-          this.sampleIndex = 0
-          this.pitch = e.data.pitch
+          this.currentOscillator = this.oscillators[e.data.pitch]
+          this.currentOscillator?.play()
           break
         case "noteOff":
-          this.isEnabled = false
+          this.currentOscillator?.stop()
           break
         case "loadSample":
-          this.samples[e.data.pitch] = e.data.data
-          this.sampleIndex = 0
+          this.oscillators[e.data.pitch] = new WavetableOscillator(e.data.data)
           break
       }
     }
   }
 
   process(inputs, outputs) {
-    if (!this.isEnabled) {
-      return true
-    }
-    const sample = this.samples[this.pitch]
     const output = outputs[0][0]
-
-    for (let i = 0; i < output.length; ++i) {
-      output[i] = sample[this.sampleIndex++]
-      if (this.sampleIndex >= sample.length) {
-        this.sampleIndex = 0
-      }
-    }
-
+    this.currentOscillator?.process(output)
     return true
   }
 }
