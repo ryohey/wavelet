48b1d660745ada76883413ac0ec6f55a7a5cb104
ryohey <info@codingcafe.jp>
2021-09-18 18:04:49 +0900

    Add @types/audioworklet

--- src/@types/AudioWorkletGlobalScope.d.ts
+++
@@ -1,6 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletGlobalScope#properties
-export declare global {
-  var currentFrame: number
-  var currentTime: number
-  var sampleTime: number
-}
