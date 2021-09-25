eb131d54c320f9a99c44078d3b23d4a7a72c90ca
ryohey <info@codingcafe.jp>
2021-09-25 18:26:24 +0900

    Load all sounds

--- src/soundfont/loader.ts
+++ src/soundfont/loader.ts
@@ -43,10 +43,6 @@ export const loadSoundFontSamples = async function* (
       globalInstrumentZone = instrumentZones[0]
     }
 
-    if (presetHeader.bank !== 0 || presetHeader.preset !== 2) {
-      continue
-    }
-
     for await (const zone of instrumentZones.filter(
       (zone) => zone.sampleID !== undefined
     )) {
