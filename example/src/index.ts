2d65b32a47522265f1ed5fe6731f6eabffb004f3
ryohey <info@codingcafe.jp>
2021-10-10 14:48:59 +0900

    Remove canvas

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,55 +3,6 @@ import { read } from "midifile-ts"
 import { midiMessageToSynthEvent } from "./midiMessageToSynthEvent"
 import { playMIDI } from "./playMIDI"
 
-class MatrixCanvas {
-  private canvas: HTMLCanvasElement
-  private ctx: CanvasRenderingContext2D
-  private valueToColor: (value: number) => string
-
-  readonly matrix = Array.from({ length: 128 }, () =>
-    Array.from({ length: 128 }, () => 0)
-  )
-
-  constructor(
-    canvas: HTMLCanvasElement,
-    valueToColor: (value: number) => string
-  ) {
-    this.canvas = canvas
-    this.valueToColor = valueToColor
-    const ctx = canvas.getContext("2d")
-    if (ctx === null) {
-      throw new Error("Failed to getContext")
-    }
-    this.ctx = ctx
-  }
-
-  draw() {
-    const { canvas, ctx, matrix } = this
-    const scale = window.devicePixelRatio
-    const width = canvas.width * scale
-    const height = canvas.height * scale
-    const cellWidth = width / matrix[0].length
-    const cellHeight = (height * scale) / matrix.length
-    ctx.clearRect(0, 0, width, height)
-    matrix.forEach((row, y) => {
-      row.forEach((value, x) => {
-        ctx.fillStyle = this.valueToColor(value)
-        ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight)
-      })
-    })
-  }
-}
-
-const loadStateCanvas = new MatrixCanvas(
-  document.getElementById("load-canvas") as HTMLCanvasElement,
-  (value) => `rgba(0, 255, 0, ${value / 3})`
-)
-
-const noteCanvas = new MatrixCanvas(
-  document.getElementById("note-canvas") as HTMLCanvasElement,
-  (value) => `rgba(0, 0, 255, ${value / 127})`
-)
-
 const main = async () => {
   const context = new AudioContext()
   let synth: AudioWorkletNode
@@ -109,28 +60,13 @@ const main = async () => {
         const e = midiMessageToSynthEvent(event.data, channel)
         if (e !== null) {
           postSynthMessage(e)
-          drawMidiMessage(e)
         }
       }
     })
   }
 
-  const drawMidiMessage = (e: SynthEvent) => {
-    switch (e.type) {
-      case "noteOn":
-        noteCanvas.matrix[e.channel][e.pitch] = 50
-        break
-      case "noteOff":
-        // noteCanvas.matrix[e.channel][e.pitch] = 0
-        break
-    }
-    noteCanvas.draw()
-  }
-
   await setup()
 
-  // loadMIDIjsSoundFont().catch((e) => console.error(e))
-  // loadWaveletSound().catch((e) => console.error(e))
   loadSoundFont().catch((e) => console.error(e))
   setupMIDIInput().catch((e) => console.error(e))
 
@@ -145,7 +81,6 @@ const main = async () => {
       const midi = read(reader.result as ArrayBuffer)
       playMIDI(midi, context.sampleRate, (e: SynthEvent) => {
         postSynthMessage(e)
-        // drawMidiMessage(e)
       })
     }
     const input = e.currentTarget as HTMLInputElement
