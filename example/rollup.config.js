770c284186f591cd78b4c89691a42a2565fd9ce5
ryohey <info@codingcafe.jp>
2021-10-17 14:37:42 +0900

    Monorepo

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,3 +1,4 @@
+import copy from "@barusu/rollup-plugin-copy"
 import commonjs from "@rollup/plugin-commonjs"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import rollupTypescript from "@rollup/plugin-typescript"
@@ -15,6 +16,14 @@ export default {
     nodeResolve({ preferBuiltins: false, browser: true }),
     commonjs(),
     rollupTypescript(),
+    copy({
+      targets: [
+        {
+          src: "../node_modules/@ryohey/wavelet/dist/processor.*",
+          dest: "public/js",
+        },
+      ],
+    }),
     serve({
       contentBase: "public",
       open: true,
@@ -24,4 +33,17 @@ export default {
       },
     }),
   ],
+}
+
+function copyWorker(src, dst) {
+  // https://lihautan.com/12-line-rollup-plugin/
+  return {
+    name: "copy-worker",
+    load() {
+      this.addWatchFile(src)
+    },
+    generateBundle() {
+      fs.copyFileSync(src, dst)
+    },
+  }
 }
