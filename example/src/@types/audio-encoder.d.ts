5dd9bf811770d978434adaf73873862324e29596
ryohey <info@codingcafe.jp>
2021-12-05 17:17:51 +0900

    Fix

--- example/src/@types/audio-encoder.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "audio-encoder" {
-  function encode(
-    audioBuffer: AudioBuffer,
-    encoding: "WAV" | number,
-    onProgress: (progress: number) => void,
-    onComplete: (blob: Blob) => void
-  )
-}
